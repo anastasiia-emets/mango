1b4b1d1859cdd2c1a46c4ed4d03d31fd90d95d11
Anastasiia <nastia4019@gmail.com>
2019-07-05 14:17:35 +0300

    d

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,23 +1,20 @@
- const el = document.querySelector("#module");
+//  const el = document.querySelector(".paralax");
 
- el.addEventListener("mousemove", (e) => {
-   el.style.backgroundPositionX = -e.offsetX + "px";
-   el.style.backgroundPositionY = -e.offsetY + "px";
- });
+//  el.addEventListener("mousemove", (e) => {
+//    el.style.backgroundPositionX = -e.offsetX + "px";
+//    el.style.backgroundPositionY = -e.offsetY + "px";
+//  });
 
-$("#menu-button").click(function(){
+$(".main-block__humb").click(function(){
  
   $(this).toggleClass("active");
-  $("#menu-button").toggleClass("unbutton");
+  $(".main-block__humb").toggleClass("unbutton");
   $("#menu").slideToggle("slow");
   $("#menu").css("display","flex")
   $(".main-block-item1").toggleClass("unhover");
   $(".main-block__links").toggleClass("unhover");
   $(".main-block-item2").toggleClass("main-block-item2_hover");
   });
-
-
-
 
 $(document).ready(function(){
   $.fn.animate_Text = function() {
@@ -30,8 +27,8 @@ $(document).ready(function(){
     });
    });
   };
-  $('#example').show();
-  $('#example').animate_Text();
+  $('.main-block-text__description').show();
+  $('.main-block-text__description').animate_Text();
  });
 
  $(document).ready(function(){
@@ -39,3 +36,34 @@ $(document).ready(function(){
   $('a').fadeIn(5000);
 });
 
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
